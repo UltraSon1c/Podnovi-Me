ac7b58e85b4db5243d0884b0b7212003c260746f
Mitko Gurbanski <admin@podnovi.me>
2017-10-06 16:08:29 +0000

    Removed some Plugins

--- plugins/amp-category-base-remover-1.0.1/uninstall.php
+++
@@ -1,9 +0,0 @@
-<?php
-// If uninstall not called from WordPress, then exit
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-	exit;
-}
-
-remove_filter('category_rewrite_rules', 'ampforwp_remove_category_url_rewrite_rules');
-global $wp_rewrite;
-$wp_rewrite->flush_rules();
