90086fbacb493509dcb91d2c698a623cf7f9c56c
Mitko Gurbanski <admin@podnovi.me>
2017-10-06 16:13:17 +0000

    Added config files

---
+++ weeblramp_logs/weeblramp/debug/log_debug.2017-07-31.log.php
@@ -0,0 +1,61 @@
+<?php
+// wbLib log file			
+defined('WPINC') || die(__FILE__);
+
+DATE	TIME	TYPE	IP	USER	MESSAGE
+2017-07-31	18:36:23	debug	172.68.51.176	guest	WeeblrampHelper_Boot::init::270:  - Set rewriterules for post types: Array
+(
+    [post] => Array
+        (
+            [enabled] => 1
+        )
+
+    [page] => Array
+        (
+            [enabled] => 1
+        )
+
+)
+
+2017-07-31	18:36:23	debug	172.68.51.176	guest	WeeblrampHelper_Boot::init::270:  - Set rewriterules for post types: Array
+(
+    [post] => Array
+        (
+            [enabled] => 1
+        )
+
+    [page] => Array
+        (
+            [enabled] => 1
+        )
+
+)
+
+2017-07-31	18:36:23	debug	172.68.51.176	guest	WeeblrampHelper_Boot::init::283:  - Flushed rewrite rules and reset weeblramp_rewrite_rules_flush_required to 0
+2017-07-31	18:36:30	debug	172.68.51.176	guest	WeeblrampHelper_Boot::init::270:  - Set rewriterules for post types: Array
+(
+    [post] => Array
+        (
+            [enabled] => 1
+        )
+
+    [page] => Array
+        (
+            [enabled] => 1
+        )
+
+)
+
+2017-07-31	18:36:45	debug	172.68.51.176	guest	WeeblrampHelper_Boot::init::270:  - Set rewriterules for post types: Array
+(
+    [post] => Array
+        (
+            [enabled] => 1
+        )
+
+    [page] => Array
+        (
+            [enabled] => 1
+        )
+
+)
---
+++ weeblramp_logs/weeblramp/errors/log_errors.2017-07-31.log.php
@@ -0,0 +1,7 @@
+<?php
+// wbLib log file			
+defined('WPINC') || die(__FILE__);
+
+DATE	TIME	TYPE	IP	USER	MESSAGE
+2017-07-31	18:35:17	error	172.68.51.176	guest	WeeblrampHelper_Boot::activate::503 Activation error: <ul><li><p>The following plugin(s) are already active, but not compatible with weeblrAMP:</p><ul><li>WordPress AMP plugin</li><li>WooComerce AMP plugin</li></ul><p>Please disable them before trying to activate weeblrAMP again.</p></li></ul>
+2017-07-31	18:35:45	error	172.68.51.176	guest	WeeblrampHelper_Boot::activate::503 Activation error: <ul><li><p>The following plugin(s) are already active, but not compatible with weeblrAMP:</p><ul><li>WordPress AMP plugin</li></ul><p>Please disable them before trying to activate weeblrAMP again.</p></li></ul>
